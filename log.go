3ce306897117b05e29b4f8bdf2a1c73b767ac298
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-06-19 04:07:25 +0200

    Rename errorLogger interface

--- log.go
+++ log.go
@@ -1,23 +1,23 @@
 package emperror
 
-// LogHandler accepts an errorLogger instance and logs an error.
+// LogHandler accepts an logger instance and logs an error.
 //
 // Compatible with most level-based loggers.
 type LogHandler struct {
-	logger errorLogger
+	l logger
 }
 
-// errorLogger covers most of the level-based logging solutions.
-type errorLogger interface {
+// logger covers most of the level-based logging solutions.
+type logger interface {
 	Error(args ...interface{})
 }
 
 // NewLogHandler returns a new LogHandler.
-func NewLogHandler(logger errorLogger) Handler {
-	return &LogHandler{logger}
+func NewLogHandler(l logger) Handler {
+	return &LogHandler{l}
 }
 
 // Handle takes care of an error by logging it.
 func (h *LogHandler) Handle(err error) {
-	h.logger.Error(err)
+	h.l.Error(err)
 }
