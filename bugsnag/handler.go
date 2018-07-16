4b5ee3f4d2d85cc0888fc3a00c48efb94829ac95
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2018-07-16 11:04:26 +0200

    Improve docs

--- bugsnag/handler.go
+++ bugsnag/handler.go
@@ -2,26 +2,11 @@
 Package bugsnag provides Bugsnag integration.
 
 Bugsnag recommends to create a global scope error handler.
-Although that works, makes writing software harder,
-so this package recommends creating a separate instance:
+Although that works, makes writing software harder, so this package recommends creating a separate instance:
 
 	APIKey := "key"
 
 	handler := bugsnag.NewHandler(APIKey)
-
-If you need access to the underlying Notifier instance (or need more advanced construction), you can access it from the handler:
-
-	handler.Notifier.Config.AppVersion = "1.0.0"
-
-Bugsnag provides an extensive set of configuration options, so it might make sense to manually construct it:
-
-	// Note: there is a conflict in package names which we resolved with the "_" (underline) prefix.
-	handler := &_bugsnag.Handler {
-		Notifier: bugsnag.New(bugsnag.Configuration{
-			APIKey:      APIKey,
-			Synchronous: true,
-		})
-	}
 */
 package bugsnag
 
