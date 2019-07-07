b5a7154e65216bc2e0b0aef4b37c24b50a1e457a
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-07-07 22:49:07 +0200

    Reduce the number of panic related files

--- panic.go
+++ panic.go
@@ -1,5 +1,10 @@
 package emperror
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Panic panics if the passed error is not nil.
 // If the error does not contain any stack trace, the function attaches one, starting from the frame of the
 // "Panic" function call.
@@ -15,5 +20,38 @@ func Panic(err error) {
 		}
 
 		panic(err)
+	}
+}
+
+// Recover accepts a recovered panic (if any) and converts it to an error (if necessary).
+func Recover(r interface{}) (err error) {
+	if r != nil {
+		switch x := r.(type) {
+		case string:
+			err = errors.New(x)
+		case error:
+			err = x
+		default:
+			err = fmt.Errorf("unknown panic, received: %v", r)
+		}
+
+		if _, ok := StackTrace(err); !ok {
+			err = &wrappedError{
+				err:   err,
+				stack: callers()[2:], // TODO: improve callers?
+			}
+		}
+	}
+
+	return err
+}
+
+// HandleRecover recovers from a panic and handles the error.
+//
+// 		defer emperror.HandleRecover(errorHandler)
+func HandleRecover(handler Handler) {
+	err := Recover(recover())
+	if err != nil {
+		handler.Handle(err)
 	}
 }
--- panic_test.go
+++ panic_test.go
@@ -53,3 +53,86 @@ func TestPanic_NoError(t *testing.T) {
 
 	Panic(nil)
 }
+
+func createRecoverFunc(p interface{}) func() error {
+	return func() (err error) {
+		defer func() {
+			err = Recover(recover())
+		}()
+
+		panic(p)
+	}
+}
+
+func assertRecoveredError(t *testing.T, err error, msg string) {
+	t.Helper()
+
+	st, ok := StackTrace(err)
+	if !ok {
+		t.Fatal("error is expected to carry a stack trace")
+	}
+
+	if got, want := fmt.Sprintf("%n", st[0]), "createRecoverFunc.func1"; got != want { // nolint: govet
+		t.Errorf("function name does not match the expected one\nactual:   %s\nexpected: %s", got, want)
+	}
+
+	if got, want := fmt.Sprintf("%s", st[0]), "panic_test.go"; got != want { // nolint: govet
+		t.Errorf("file name does not match the expected one\nactual:   %s\nexpected: %s", got, want)
+	}
+
+	if got, want := fmt.Sprintf("%d", st[0]), "63"; got != want { // nolint: govet
+		t.Errorf("line number does not match the expected one\nactual:   %s\nexpected: %s", got, want)
+	}
+
+	if got, want := err.Error(), msg; got != want {
+		t.Errorf("error message does not match the expected one\nactual:   %s\nexpected: %s", got, want)
+	}
+}
+
+func TestRecover_ErrorPanic(t *testing.T) {
+	err := fmt.Errorf("internal error")
+
+	f := createRecoverFunc(err)
+
+	v := f()
+
+	assertRecoveredError(t, v, "internal error")
+}
+
+func TestRecover_StringPanic(t *testing.T) {
+	f := createRecoverFunc("internal error")
+
+	v := f()
+
+	assertRecoveredError(t, v, "internal error")
+}
+
+func TestRecover_AnyPanic(t *testing.T) {
+	f := createRecoverFunc(123)
+
+	v := f()
+
+	assertRecoveredError(t, v, "unknown panic, received: 123")
+}
+
+func TestRecover_Nil(t *testing.T) {
+	f := createRecoverFunc(nil)
+
+	v := f()
+
+	if got, want := v, error(nil); got != want { // nolint: govet
+		t.Errorf("the recovered value is expected to be nil\nactual: %v", got)
+	}
+}
+
+func TestHandleRecover(t *testing.T) {
+	err := errors.New("error")
+
+	defer HandleRecover(HandlerFunc(func(err error) {
+		if got, want := err.Error(), "error"; got != want {
+			t.Errorf("error does not match the expected one\nactual:   %s\nexpected: %s", got, want)
+		}
+	}))
+
+	panic(err)
+}
