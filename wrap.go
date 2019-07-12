db43ae8811252756a4da3cfca32e542c69c16cfd
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-07-12 15:39:25 +0200

    Move wrap functionality in a single file

--- wrap.go
+++ wrap.go
@@ -23,3 +23,24 @@ func Wrap(err error, message string) error {
 func Wrapf(err error, format string, args ...interface{}) error {
 	return errors.WithStackDepthIf(errors.WithMessagef(err, format, args...), 1)
 }
+
+// WrapWith returns an error annotating err with a stack trace
+// at the point Wrap is called (if there is none attached to the error yet), the supplied message,
+// and the supplied context.
+// If err is nil, Wrap returns nil.
+//
+// Note: do not use this method when passing errors between goroutines.
+func WrapWith(err error, message string, keyvals ...interface{}) error {
+	if err == nil {
+		return nil
+	}
+
+	err = errors.WithStackDepthIf(errors.WithMessage(err, message), 1)
+
+	// Attach context to the error
+	if len(keyvals) > 0 {
+		err = With(err, keyvals...)
+	}
+
+	return err
+}
