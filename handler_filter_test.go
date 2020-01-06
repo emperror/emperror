d4d15bd9f668a201b8899ca52a0f51ae75e3cf71
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2020-01-06 20:18:07 +0100

    Fix filter example

--- handler_filter_test.go
+++ handler_filter_test.go
@@ -11,22 +11,13 @@ func ExampleWithFilter() {
 	err := errors.New("no more errors")
 	isErr := errors.New("is")
 
-	type asError struct {
-		error
-	}
-	asErr := asError{errors.New("as")}
-
 	handler := WithFilter(
 		HandlerFunc(func(err error) { fmt.Println(err) }),
-		match.Any{
-			match.Is(isErr),
-			match.As(&asError{}),
-		},
+		match.Is(isErr),
 	)
 
 	handler.Handle(err)
 	handler.Handle(isErr)
-	handler.Handle(asErr)
 
 	// Output: no more errors
 }
